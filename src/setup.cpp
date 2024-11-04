643cd88a903d9085b47c094081694182740996d5
JSmart <jtrentsmart@gmail.com>
2024-11-04 15:58:27 -0600

    Playing with units
    si_u and si_rho are actually in the lbm units - need to set an lbm.units construct to convert between them

--- src/setup.cpp
+++ src/setup.cpp
@@ -9,15 +9,15 @@ void main_setup() { // Airfoil Test; required extensions in defines.hpp: D2Q9, F
     const uint y_length = 1024u;
     const uint z_length = 1u;
     const float c = float(x_length)/20.0f;
-    const float t = 0.10f * c;
-    const float Re = 250.0f;
-    const float si_u = 0.10f;
+    const float t = 0.12f * c;
+    const float Re = 2e5;
+    const float si_u = 0.05f;
     const float si_rho = 1.0f;
     const float p = 0.40f;
-    const float m = 0.06f;
-    const float aoa = 45.0f;
+    const float m = 0.02f;
+    const float aoa = 10.0f;
     const float si_A = x_length*y_length;
-    LBM lbm(x_length, y_length, z_length, units.nu_from_Re(Re, y_length, si_u));
+    LBM lbm(x_length, y_length, z_length, units.nu_from_Re(Re, c, si_u));
     // ###################################################################################### define geometry ######################################################################################
     const uint Nx=lbm.get_Nx(), Ny=lbm.get_Ny(), Nz=lbm.get_Nz(); parallel_for(lbm.get_N(), [&](ulong n) { uint x=0u, y=0u, z=0u; lbm.coordinates(n, x, y, z);
         if(airfoil_cam(x, y, z, float3(Nx/2u, Ny/2u, Nz/2u), c, t, p, m, aoa)) {lbm.flags[n] = TYPE_S;}
