ac464b9b0a25efc374ca2d142d1fa334e779e94b
JSmart <jtrentsmart@gmail.com>
2024-11-06 14:37:52 -0600

    Units Fix

--- src/setup.cpp
+++ src/setup.cpp
@@ -3,25 +3,35 @@
 
 void main_setup() { // Airfoil Test; required extensions in defines.hpp: D2Q9, FP16C, EQUILIBRIUM_BOUNDARIES, INTERACTIVE_GRAPHICS, SUBGRID, FORCE_FIELD
     // ################################################################## define simulation box size, viscosity and volume force ###################################################################
-    const uint memory = 200u;
+    // LBM Parameters
+    const uint memory = 20000u;
     const uint T_Steps = 15000u;
     const uint x_length = 2048u;
     const uint y_length = 1024u;
     const uint z_length = 1u;
+
+    //Flow Parameters
+    const float Re = 6e6f;
+    const float si_u = 88.8f;
+    const float si_rho = 1.225f;
+
+
+    //Airfoil Parameters
     const float c = float(x_length)/20.0f;
-    const float t = 0.12f * c;
-    const float Re = 2e5;
-    const float si_u = 0.05f;
-    const float si_rho = 1.0f;
-    const float p = 0.40f;
-    const float m = 0.02f;
-    const float aoa = 10.0f;
-    const float si_A = x_length*y_length;
+    const float t = 0.06f * c;
+
+    const float p = 0.00f;
+    const float m = 0.00f;
+    const float aoa = 5.0f;
+
+    //Setup Units
+    units.set_m_kg_s(c, 0.05f, 1.0f, 1.0f, si_u, si_rho);
+
     LBM lbm(x_length, y_length, z_length, units.nu_from_Re(Re, c, si_u));
     // ###################################################################################### define geometry ######################################################################################
     const uint Nx=lbm.get_Nx(), Ny=lbm.get_Ny(), Nz=lbm.get_Nz(); parallel_for(lbm.get_N(), [&](ulong n) { uint x=0u, y=0u, z=0u; lbm.coordinates(n, x, y, z);
         if(airfoil_cam(x, y, z, float3(Nx/2u, Ny/2u, Nz/2u), c, t, p, m, aoa)) {lbm.flags[n] = TYPE_S;}
-        else lbm.u.x[n] = si_u;
+        else lbm.u.x[n] = 0.05f;
         if(x==0u||x==Nx-1u||y==0u||y==Ny-1u) lbm.flags[n] = TYPE_E; // all non periodic
     }); // ####################################################################### run simulation, export images and data ##########################################################################
     lbm.graphics.visualization_modes = VIS_FIELD;
@@ -35,9 +45,8 @@ void main_setup() { // Airfoil Test; required extensions in defines.hpp: D2Q9, F
         lbm.calculate_force_on_boundaries();
         const float3 lbm_force = lbm.calculate_force_on_object(TYPE_S);
         const float force = units.si_F(lbm_force.x);
-        const float Cd = units.si_F(lbm_force.x) / (0.5f * si_rho * sq(si_u) * si_A); // expect Cd to be too large by a factor 1.3-2.0x; need wall model                                                                         ");
+        const float Cd = units.si_F(lbm_force.x) / (0.5f * si_rho * sq(si_u)); // expect Cd to be too large by a factor 1.3-2.0x; need wall model                                                                         ");
         write_line(path + "Cd.dat", to_string(lbm.get_t()) + "\t" + to_string(Cd, 3u) + "\n");
     }
-    lbm.flags.write_device_to_vtk();
-    lbm.write_status(path);
+
 }
